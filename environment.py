208188d0686714a4c479e591cd2d262e0c1829e0
Aidan Barbieux <abarbieu@calpoly.edu>
2022-10-26 22:20:13 -0700

    started transition to env class

--- environment.py
+++ environment.py
@@ -1,23 +1,91 @@
-import numpy as np 
+import numpy as np
 
 from scipy.stats import uniform
 from scipy.stats import levy_stable
 
 
-# put x in range -w/2 to w/2
-def norm_center(x,w):
-    x -= x.min()
-    x *= w/x.max()
-    return x
+class environment:
+    # ----- Neighborhood/Channel Parameters -----
+    moore_f = (np.array([1,  1,  1,  0,  0,  -1, -1, -1]),
+               np.array([-1,  0,  1, -1,  1,  -1,  0,  1]))  # Moore neigh indices
 
-def get_levy_dust(shape, points, alpha, beta):
-    # uniformly distributed angles
-    angle = uniform.rvs(size=(points,), loc=.0, scale=2.*np.pi )
+    von_n = (np.array([-1,  0,  0, 1]),
+             np.array([0, -1,  1,  0]))  # Von Neumann neighborhood indices
 
-    # Levy distributed step length
-    r = abs(levy_stable.rvs(alpha, beta, size=points))
+    moore_f = (np.array([1,  1,  1,  0,  0,  0,  -1, -1, -1]),
+               np.array([-1,  0,  1, -1,  0,  1,  -1,  0,  1]))  # Includes center
 
-    x = norm_center(np.cumsum(r * np.cos(angle)), shape[0]-1)
-    y = norm_center(np.cumsum(r * np.sin(angle)), shape[1]-1)
-    
-    return np.array([x,y])
+    von_n_f = (np.array([-1,  0,  0,  0, 1]),
+               np.array([0, -1,  0,  1,  0]))  # Includes center
+
+    def __init__(self):
+        self.life_i = 0  # Channel index of life
+        self.food_i = 1  # Channel index of food
+        self.resv_i = 2  # Channel index of resevoir
+        self.hidden_i = 3  # Index of first hidden channel
+
+        self.kernel = self.von_n
+        self.kernel_full = self.von_n_f  # Incliudes center
+        self.n_neighs = len(self.kernel[0])
+        self.n_hidden = 4
+        self.n_channels = 3 + self.n_hidden
+
+        self.esize = 32  # Width/Height of environment
+        self.eshape = (self.n_channels, self.esize, self.esize)
+
+        self.alpha = 1  # Alpha for levy food distribution
+        self.beta = 1  # Beta for levy food distribution
+        self.food_amt = 16  # Number of food points created
+        self.max_food = 4  # Densest food source -- necessary?
+
+        # TODO: explore larger perceptive fields? sobel filters?
+        # Center + neighs, all channels
+        self.input_shape = (1 + self.n_neighs) * self.n_channels
+        # Draw from neighs, hidden channels for center
+        self.output_shape = self.n_neighs + self.n_hidden
+
+        self.life_cost = 0.3  # How much life is depleted per time step
+        self.min_life = 0.1  # Life below this is treated as dead
+        self.life_transfer_rate = 0.2  # Amount of food transferrable by 1 unit of life
+        self.food_transfer_rate = 1  # Amount of life transferrable by 1 unit of food
+        self.max_life = 1
+
+        # put x in range -w/2 to w/2
+
+        def norm_center(x, w):
+            x -= x.min()
+            x *= w/x.max()
+            return x
+
+        def get_levy_dust(shape: tuple, points: int, alpha: float, beta: float) -> np.array:
+            # uniformly distributed angles
+            angle = uniform.rvs(size=(points,), loc=.0, scale=2.*np.pi)
+
+            # Levy distributed step length
+            r = abs(levy_stable.rvs(alpha, beta, size=points))
+
+            x = norm_center(np.cumsum(r * np.cos(angle)), shape[0]-1)
+            y = norm_center(np.cumsum(r * np.sin(angle)), shape[1]-1)
+
+            return np.array([x, y])
+
+        def gen_padded_env(pad=1) -> None:
+            shape = (self.eshape[1], self.eshape[2])
+            env = np.zeros(self.eshape)
+
+            dust = get_levy_dust(
+                (shape[0]-pad*2, shape[1]-pad*2), self.food_amt, self.alpha, self.beta).T
+            dust = np.array(dust, dtype=np.int64)
+            dust, density = np.unique(dust, axis=0, return_counts=True)
+
+            env[self.food_i, dust[:, 0]+1, dust[:, 1]+1] = density
+            return env
+
+        # Innoculate the cell with the most food - this is likely to be surrounded
+        # by more food given def of levy dust
+
+        def innoculate_env(env):
+            maxfood = np.unravel_index(
+                np.argmax(env[self.food_i]), env[self.food_i].shape)
+            env[self.life_i][maxfood[0], maxfood[1]] = 1
+            return env
--- graphics.py
+++ graphics.py
@@ -1,10 +1,13 @@
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-import PIL.Image, PIL.ImageDraw
+import PIL.Image
+import PIL.ImageDraw
 import numpy as np
 from IPython.display import Video, Image, HTML, clear_output
 from moviepy.video.io.ffmpeg_writer import FFMPEG_VideoWriter
+
+import tensorflow as tf
 
 # def np2pil(a):
 #   if a.dtype in [np.float32, np.float64]:
@@ -14,7 +17,7 @@ from moviepy.video.io.ffmpeg_writer import FFMPEG_VideoWriter
 # def imwrite(f, a, fmt=None):
 #   a = np.asarray(a)
 #   if isinstance(f, str):
-#     fmt = f.rsplit('.', 1)[-1].lower()
+#     fmt = f.rsplit('.',s 1)[-1].lower()
 #     if fmt == 'jpg':
 #       fmt = 'jpeg'
 #     f = open(f, 'wb')
@@ -48,101 +51,109 @@ from moviepy.video.io.ffmpeg_writer import FFMPEG_VideoWriter
 #   a = np.rollaxis(a, 2, 1).reshape([th*h, tw*w]+list(a.shape[4:]))
 #   return a
 
+
 def zoom(img, scale=4):
-  img = np.repeat(img, scale, 0)
-  img = np.repeat(img, scale, 1)
-  return img
+    img = np.repeat(img, scale, 0)
+    img = np.repeat(img, scale, 1)
+    return img
+
 
 class VideoWriter:
-  def __init__(self, filename=None, fps=30.0, **kw):
-    self.writer = None
-    self.params = dict(filename=filename, fps=fps, **kw)
+    def __init__(self, filename=None, fps=30.0, **kw):
+        self.writer = None
+        self.params = dict(filename=filename, fps=fps, **kw)
 
-  def add(self, img):
-    img = np.asarray(img)
-    if self.writer is None:
-      h, w = img.shape[:2]
-      self.writer = FFMPEG_VideoWriter(size=(w, h), **self.params)
-    if img.dtype in [np.float32, np.float64]:
-      img = np.uint8(img.clip(0, 1)*255)
-    if len(img.shape) == 2:
-      img = np.repeat(img[..., None], 3, -1)
-    self.writer.write_frame(img)
+    def add(self, img):
+        img = np.asarray(img)
+        if self.writer is None:
+            h, w = img.shape[:2]
+            self.writer = FFMPEG_VideoWriter(size=(w, h), **self.params)
+        if img.dtype in [np.float32, np.float64]:
+            img = np.uint8(img.clip(0, 1)*255)
+        if len(img.shape) == 2:
+            img = np.repeat(img[..., None], 3, -1)
+        self.writer.write_frame(img)
 
-  def close(self):
-    if self.writer:
-      self.writer.close()
+    def close(self):
+        if self.writer:
+            self.writer.close()
 
-  def __enter__(self):
-    return self
+    def __enter__(self):
+        return self
 
-  def __exit__(self, *kw):
-    self.close()
-  
-# def to_alpha(x):
-#   return tf.clip_by_value(x[..., 3:4], 0.0, 1.0)
+    def __exit__(self, *kw):
+        self.close()
+
+
+def to_alpha(x):
+    return tf.clip_by_value(x[..., 3:4], 0.0, 1.0)
+
 
 def to_rgb(x):
-  # assume rgb premultiplied by alpha
-  rgb, a = x[..., :3], to_alpha(x)
-  return 1.0-a+rgb
+    # assume rgb premultiplied by alpha
+    rgb, a = x[..., :3], to_alpha(x)
+    return 1.0-a+rgb
 
 
-#@title Utils to display environment
+# @title Utils to display environment
 
 # TODO: Show the color bar to get a sense of scale
 def ch_image(env, channel, cmap=None):
-  if cmap is None:
-    if channel == 1:
-      cmap = cm.copper
-    elif channel == 0:
-      cmap = cm.gray
-    else:
-      cmap = cm.hot
+    if cmap is None:
+        if channel == 1:
+            cmap = cm.copper
+        elif channel == 0:
+            cmap = cm.gray
+        else:
+            cmap = cm.hot
 
-  norm = mpl.colors.Normalize(env[channel].min(),env[channel].max())
-  m = cm.ScalarMappable(norm = norm, cmap = cmap)
-  return m.to_rgba(env[channel])
+    norm = mpl.colors.Normalize(env[channel].min(), env[channel].max())
+    m = cm.ScalarMappable(norm=norm, cmap=cmap)
+    return m.to_rgba(env[channel])
+
 
 def ch_plot(env, channel, cmap=None):
-  if cmap is None:
-    if channel == 1:
-      cmap = cm.copper
-    elif channel == 0:
-      cmap = cm.gray
-    else:
-      cmap = cm.hot
+    if cmap is None:
+        if channel == 1:
+            cmap = cm.copper
+        elif channel == 0:
+            cmap = cm.gray
+        else:
+            cmap = cm.hot
 
-  plt.imshow(env[channel], cmap=cmap, interpolation='nearest')
-  plt.colorbar()
-  plt.show()
-
-def ch_plimage(env,channel,cmap=cm.hot):
-  if cmap is None:
-    if channel == 1:
-      cmap = cm.copper
-    elif channel == 0:
-      cmap = cm.gray
-    else:
-      cmap = cm.hot
-
-  plt.clf()
-  fig = plt.gcf();
-  plt.imshow(env[channel], cmap=cmap, interpolation='nearest')
-  plt.colorbar()
-  fig = plt.gcf()
-  fig.canvas.draw()
-  # Now we can save it to a numpy array.
-  data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
-  data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-  # buf = io.BytesIO()
-  # plt.savefig(buf)
-  # buf.seek(0)
-  return data
+    plt.imshow(env[channel], cmap=cmap, interpolation='nearest')
+    plt.colorbar()
+    plt.show()
 
 
-def gen_vid(frames, fname="test.mp4"):
-  with VideoWriter(fname) as vid:
-      for f in frames:
-        vid.add(to_rgb(zoom(np.array(f),256/frames.shape[2])))
-  return fname
+def ch_plimage(env, channel, cmap=cm.hot):
+    if cmap is None:
+        if channel == 1:
+            cmap = cm.copper
+        elif channel == 0:
+            cmap = cm.gray
+        else:
+            cmap = cm.hot
+
+    plt.clf()
+    fig = plt.gcf()
+    plt.imshow(env[channel], cmap=cmap, interpolation='nearest')
+    plt.colorbar()
+    fig = plt.gcf()
+    fig.canvas.draw()
+    # Now we can save it to a numpy array.
+    data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+    data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    # buf = io.BytesIO()
+    # plt.savefig(buf)
+    # buf.seek(0)
+    return data
+
+
+def gen_vid(frames, scale=None, fname="test.mp4"):
+    if scale is None:
+        scale = 256/frames.shape[2]
+    with VideoWriter(fname) as vid:
+        for f in frames:
+            vid.add(to_rgb(zoom(np.array(f), scale)))
+    return fname
