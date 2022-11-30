066cbf2a2320dc500e5530f1e458a2683312c197
Aidan Barbieux <abarbieu@calpoly.edu>
2022-11-29 20:12:08 -0800

    Diffusion examples in small grids

--- encasm/env.py
+++ encasm/env.py
@@ -1,6 +1,7 @@
 import numpy as np
 import io
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap 
 from configparser import SafeConfigParser
 import encasm.utils as utils
 
@@ -9,7 +10,7 @@ class PetriDish:
     """A Petri dish is a multi channeled container for populations of cells.
 
     Attributes:
-        terrain (np.array): A 2d array (W x H) of the terrain of the environment, where:
+        map (np.array): A 2d array (W x H) of the map of the environment, where:
             0: empty
             1: food
             2: water
@@ -30,23 +31,27 @@ class PetriDish:
             beta (float): The beta parameter for the Lévy dust distribution
             pad (int): The padding for the Lévy dust distribution
     """
+   
     def __init__(self, id: str, config: SafeConfigParser):
         """Initializes a new Petri dish from a SafeConfigParser object.
 
         Parameters:
             config (SafeConfigParser): The SafeConfigParser object
         """
+         # Index indicates the value present in map
+        self.map_keys = ["empty", "food", "water", "poison", "sink"]
+        # black, green, blue, red, purple hex
+        self.map_colors = ["#000000", "#00FF00", "#0000FF", "#FF0000", "#800080"]
+        # black, pastel green, pastel blue, pastel red, pastel purple
+        # map_colors = ["#000000", "#b2f2b2", "#b2d8f2", "#f2b2b2", "#f2b2f2"]
+        # map_colors = [""#77dd77", "#aec6cf", "#ff6961", "#dda0dd"]
+
         self.id = id
         self.config = config
 
-        self.terrain_vals = {
-            "food": 1,
-            "water": 2,
-            "poison": 3,
-            "sink": 4,
-        }
-
         self.init_channels()
+
+        
     
     @classmethod
     def from_config_file(cls, id: str, config_file: str):
@@ -67,33 +72,57 @@ class PetriDish:
             env (PetriDish): The environment to copy
         """
         env = cls(new_id, other_env.config)
-        env.terrain = np.copy(other_env.food)
+        env.map = np.copy(other_env.map)
         env.life = np.copy(other_env.life)
         env.resv = np.copy(other_env.resv)
         env.hidden = np.copy(other_env.hidden)
+        return env
 
+    @classmethod
+    def from_channels(cls, id: str, channels: dict, config: SafeConfigParser):
+        """Creates a new Petri dish from a dictionary of channels.
 
+        Parameters:
+            channels (dict): A dictionary of channels
+        """
+        env = cls(id, config)
+        for ch, grid in channels.items():
+            env.set_channel(ch, grid)
+        return env
+
+    def fd(self):
+        return self.map == self.map_keys.index("food")
+
+    def wt(self):
+        return self.map == self.map_keys.index("water")
+    
+    def ps(self):
+        return self.map == self.map_keys.index("poison")
+    
+    def sk(self):
+        return self.map == self.map_keys.index("sink")
+    
     def init_channels(self):
         """Initializes the channels of the environment from the config dictionary."""
         self.width = self.config.getint("width", 32)
         self.height = self.config.getint("height", 32)
         self.n_hidden = self.config.getint("n_hidden", 4)
         self.n_channels = self.n_hidden + 3
-        self.terrain = np.zeros((self.width, self.height))
+        self.map = np.zeros((self.width, self.height))
         self.life = np.zeros((self.width, self.height))
         self.resv = np.zeros((self.width, self.height))
         self.hidden = np.zeros((self.n_hidden, self.width, self.height))
 
     def set_channel(self, ch: str, grid: np.array):
         """Sets the specified channel to the specified grid,
-           if channel is in terrain_vals, updates values present in grid.
+           if channel is in map_vals, updates values present in grid.
 
         Parameters:
             ch (str): The channel to set
             grid (np.array): The grid to set the channel to
         """
-        if ch in self.terrain_vals:
-            self.terrain[grid > 0] = self.terrain_vals[ch]
+        if ch in self.map_keys:
+            self.map[grid >= 1] = self.map_keys.index(ch)
         else:
             # Otherwise just set the channel attribute
             setattr(self, ch, grid)
@@ -112,7 +141,7 @@ class PetriDish:
         self.food += utils.discretize_levy_dust(
             dust, (self.width, self.height), self.config.getint("pad", 1))
 
-    def display(self, chs: list = ["terrain", "life", "resv"], cmaps: list = ["copper", "gray", "hot"], cols: int = 3, retbuf: bool = False):
+    def display(self, chs: list = ["map", "life", "resv"], cmaps: list = ["copper", "gray", "hot"], cols: int = 3, retbuf: bool = False):
         """Displays the specified channels of the environment.
 
         Parameters:
@@ -124,25 +153,36 @@ class PetriDish:
         # Ensures each channel has a color, repeats the last color if necessary
         if len(cmaps) < len(chs):
             cmaps += [cmaps[-1]] * (len(chs) - len(cmaps))
+        
+        if len(chs) == 1:
+            # If only one channel, just display it
+            if chs[0] == "map":
+                plt.matshow(getattr(self, chs[0]), cmap=ListedColormap(self.map_colors))
+            else:
+                plt.matshow(getattr(self, chs[0]), cmap=cmaps[0])
+        else:
+            rows = int(np.ceil(len(chs) / cols))
+            # Displays a grid of matshow subplots for each channel, with the specified colormap
+            # each plot with a title of the channel name and a colorbar
+            fig, axs = plt.subplots(rows, cols, figsize=(cols * 4, rows * 4))
+            for i, ch in enumerate(chs):
+                # adds padding between plots to account for colorbars
+                fig.subplots_adjust(hspace=0.5, wspace=0.5)
 
-        rows = int(np.ceil(len(chs) / cols))
-        # Displays a grid of matshow subplots for each channel, with the specified colormap
-        # each plot with a title of the channel name and a colorbar
-        fig, axs = plt.subplots(rows, cols, figsize=(cols * 4, rows * 4))
-        for i, ch in enumerate(chs):
-            # adds padding between plots to account for colorbars
-            fig.subplots_adjust(hspace=0.5, wspace=0.5)
-
-            ax = axs[i]
-            ax.matshow(getattr(self, ch), cmap=cmaps[i])
-            ax.set_title(self.id + ": " + ch)
-            # Creates a colorbar for each subplot that is the same size as the subplot with padding
-            fig.colorbar(ax.images[0], ax=ax, fraction=0.045)
-            # fig.colorbar(ax.images[0], ax=ax, pad=0.01)
-        if retbuf:
-            img_buf = io.BytesIO()
-            plt.savefig(img_buf, format='png')
-            return img_buf
+                ax = axs[i]
+                # If channel is map, use the map_colors
+                if ch == "map":
+                    ax.matshow(getattr(self, ch), cmap=ListedColormap(self.map_colors))
+                else:
+                    ax.matshow(getattr(self, ch), cmap=cmaps[i])
+                ax.set_title(self.id + ": " + ch)
+                # Creates a colorbar for each subplot that is the same size as the subplot with padding
+                fig.colorbar(ax.images[0], ax=ax, fraction=0.045)
+                # fig.colorbar(ax.images[0], ax=ax, pad=0.01)
+            if retbuf:
+                img_buf = io.BytesIO()
+                plt.savefig(img_buf, format='png')
+                return img_buf
 
 
 # rows = int(np.ceil(len(chs) / cols))
--- encasm/eval.py
+++ encasm/eval.py
@@ -7,7 +7,7 @@ from encasm.env import PetriDish
 import matplotlib.pyplot as plt
 
 
-terrain_types = {
+map_channels = {
     "F": "food",
     "L": "life",
     "P": "poison",
@@ -23,7 +23,7 @@ def get_env_config(folder):
         if entry.name.endswith(b".config"):
             config = configparser.ConfigParser()
             config.read(entry.path.decode())
-            #(int(config["Environment"]["width"]), int(config["Environment"]["height"])),
+            # (int(config["Environment"]["width"]), int(config["Environment"]["height"])),
             return config
 
 
@@ -43,20 +43,54 @@ def gen_env_dict(folder, config):
 
     # Pattern matches files like F_0-T_1.png where 01 would be the environment key, T would be the channel type of the image
     # These are returned in groups
-    pattern = re.compile(r"F_(\d+)-([A-Z])_(\d+).png")
+    pattern = re.compile(r"(\d+)-([A-Z])(_(\d+))?.png")
     envs = {}
     for entry in os.scandir(path=os.fsencode(folder)):
         if entry.name.endswith(b".png"):
             match = pattern.match(entry.name.decode())
             if match:
-                env_key = match.group(1) + "_" + match.group(3)
-                terrain_type = match.group(2)
-                if env_key not in envs:
-                    # Create a new environment with id env_key
-                    envs[env_key] = PetriDish(env_key, config["Environment"])
-                envs[env_key].set_channel(
-                    terrain_types[terrain_type], img_to_grid(entry.path.decode()))
-    return envs
+                map_key = match.group(1)
+                terrain_type = map_channels[match.group(2)]
+                test_n = match.group(4)
+                env_key = map_key + \
+                    ("-" + test_n if test_n else "default")
+                if map_key not in envs:
+                    envs[map_key] = {}
+                if test_n is None:
+                    if "default" not in envs[map_key]:
+                        envs[map_key]["default"] = {}
+                    envs[map_key]["default"][terrain_type] = img_to_grid(
+                        entry.path.decode())
+                else:
+                    if test_n not in envs[map_key]:
+                        envs[map_key][test_n] = {}
+                        envs[map_key][test_n]["name"] = env_key
+                        envs[map_key][test_n]["channels"] = {}
+                    envs[map_key][test_n]["channels"][terrain_type] = img_to_grid(
+                        entry.path.decode())
+
+    # For all environments, if they are missing a channel, add the default, or if no default, zeros
+    for env in envs:
+        for test in envs[env]:
+            if test == "default":
+                continue
+            for channel in map_channels.values():
+                if channel not in envs[env][test]:
+                    if "default" in envs[env] and channel in envs[env]["default"]:
+                        envs[env][test]["channels"][channel] = envs[env]["default"][channel]
+                    # else:
+                    #     envs[env][test]["channels"][channel] = np.zeros(
+                    #         (int(config["Environment"]["width"]), int(config["Environment"]["height"])))
+    # prints keys
+    dishes = {}
+    # turns envs into a dictionary of PetriDishes, with the key being the environment name
+    for env in envs:
+        for test in envs[env]:
+            if test != "default":
+                dishes[envs[env][test]["name"]] = PetriDish.from_channels(envs[env][test]["name"],
+                                                                          envs[env][test]["channels"],
+                                                                          config["Environment"])
+    return dishes
 
 
 def load_tests(folder, flat=False):
